b3854b03c9790039b9e93e700bc2786b3d43f1c3
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-05-06 00:21:45 -0700

    Actor Class update

--- Shoot!/src/actor/Actor.java
+++ Shoot!/src/actor/Actor.java
@@ -1,5 +1,46 @@
 package actor;
 
-public class Actor {
+public abstract class Actor {
 
+	
+	
+	//field
+	protected int hp;
+	protected int x;
+	protected int y;
+	protected int vx;
+	protected int vy;
+	
+	//cons
+	public Actor() {
+		hp = -1;
+	}
+	
+	public Actor(int hp, int x, int y, int vx, int vy) {
+		this.hp = hp;
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+	}
+
+	
+	
+	
+	//method
+	public void act() { //physical behavior
+		
+	}
+	
+	public void bounce() {
+		
+	}
+	
+	public void bounceOffWindow() {
+		
+	}
+	
+	public void intersects() {
+		
+	}
 }
