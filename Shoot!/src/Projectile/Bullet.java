313bb98fa0084f6bf3c441698656fdb84e4c2030
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-05-06 22:11:49 -0700

    projectile update

--- Shoot!/src/Projectile/Bullet.java
+++ Shoot!/src/Projectile/Bullet.java
@@ -2,4 +2,14 @@ package Projectile;
 
 public class Bullet extends Projectile{
 
+	private static int hp = 1;
+	
+	
+	public Bullet() {
+		super();
+	}
+	
+	public Bullet(double x, double y, double vx, double vy) {
+		super(hp, x, y, vx, vy);
+	}
 }
--- Shoot!/src/Projectile/Projectile.java
+++ Shoot!/src/Projectile/Projectile.java
@@ -1,7 +1,20 @@
 package Projectile;
 
+import java.awt.Color;
+
 import actor.Actor;
 
 public abstract class Projectile extends Actor{
 
+	public Projectile() {
+		super();
+	}
+	
+	public Projectile(int hp, double x, double y, double vx, double vy) {
+		super(hp, x, y , vx, vy);
+		setFill(Color.YELLOW);
+	}
+	
+	
+	
 }
