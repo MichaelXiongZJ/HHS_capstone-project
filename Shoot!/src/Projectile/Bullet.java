905d6bc6072d53987b4f8cd16bf09626337a7b5e
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-05-06 22:13:13 -0700

    update

--- Shoot!/src/Projectile/Bullet.java
+++ Shoot!/src/Projectile/Bullet.java
@@ -3,7 +3,7 @@ package Projectile;
 public class Bullet extends Projectile{
 
 	private static int hp = 1;
-	
+	private static double MAXSPEED;
 	
 	public Bullet() {
 		super();
@@ -11,5 +11,6 @@ public class Bullet extends Projectile{
 	
 	public Bullet(double x, double y, double vx, double vy) {
 		super(hp, x, y, vx, vy);
+		setRadius(5);
 	}
 }
