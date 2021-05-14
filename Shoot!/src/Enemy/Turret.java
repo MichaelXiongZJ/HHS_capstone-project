ec14ca9edf19a454e4551736446edadf184cb097
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-05-14 00:10:23 -0700

    comments update

--- Shoot!/src/Enemy/Turret.java
+++ Shoot!/src/Enemy/Turret.java
@@ -15,12 +15,16 @@ public class Turret extends Enemy{
 	 * A stationary enemy that shoots
 	 * @param x x coord
 	 * @param y y coord
+	 * @author Michael
 	 */
 	public Turret(double x, double y) {
 		super(5, x, y);
 		setRadius(40);
 	}
-	
+
+	/**
+	 * Turret's moveAround() don't do anything
+	 */
 	public void moveAround(int time) {
 		
 	}
--- Shoot!/src/Projectile/Projectile.java
+++ Shoot!/src/Projectile/Projectile.java
@@ -11,28 +11,57 @@ public abstract class Projectile extends Actor{
 		super();
 	}
 	
+	/**
+	 * 
+	 * @param hp starting hp
+	 * @param x x coord
+	 * @param y y coord
+	 * @param vx starting x velocity
+	 * @param vy starting y velocity
+	 * @author Michael
+	 */
 	public Projectile(int hp, double x, double y, double vx, double vy) {
 		super(hp, x, y , vx, vy);
 		setFill(Color.YELLOW);
 	}
 	
+	/**
+	 * set x coord
+	 * @author Nont
+	 */
 	public void setX(double x) {
 		super.setX(x);
 	}
 	
+	/**
+	 * set y coord
+	 * @author Nont
+	 */
 	public void setY(double y) {
 		super.setY(y);
 	}
 	
+	/**
+	 * get x coord
+	 * @author Nont
+	 */
 	public double getX() {
 		return super.getX();
 	}
 	
+	/**
+	 * get y coord
+	 * @author Nont
+	 */
 	public double getY() {
 		return super.getY();
 	}
 	
 
+	/**
+	 * Bounce bullet, and -1 hp at the same time.
+	 * @author Michael
+	 */
 	public boolean bounce() {
 		setvx(-getvx());
 		setvy(-getvy());
